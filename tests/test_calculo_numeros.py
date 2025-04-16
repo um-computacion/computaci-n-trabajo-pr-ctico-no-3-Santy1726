bea42be2d4adda5f125bbd31d1c481223804a156
Santiago Ortega <sa.ortega@alumno.um.edu.ar>
2025-04-16 00:50:02 -0300

    numeros negativos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,6 +1,7 @@
 import unittest
 from src.exceptions import ingrese_numero
 from unittest.mock import patch
+from src.exceptions import ingrese_numero, NumeroDebeSerPositivo
 
 class TestIngresoNumeroValido(unittest.TestCase):
 
@@ -23,6 +24,26 @@ class TestIngresoNumeroValido(unittest.TestCase):
     def test_ingreso_con_espacios(self, mock_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 10)
+ 
+    @patch('builtins.input', return_value='-1')
+    def test_menos_uno(self, mock_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='-100')
+    def test_menos_cien(self, mock_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='-999999')
+    def test_menos_grande(self, mock_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value=' -5 ')
+    def test_con_espacios_negativos(self, mock_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
 
 if __name__ == '__main__':
     unittest.main()
